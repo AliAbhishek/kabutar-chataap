ce0629baa01814fe4e2c6c69b85325f4926a0a06
Abhishek <mosimkhan15102001@gmail.com>
2024-09-21 22:20:49 +0530

    mongo cluster

--- Backend/DBConnection/DBconnection.js
+++ Backend/DBConnection/DBconnection.js
@@ -1,15 +1,44 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
+// import mongoose from "mongoose";
+// import dotenv from "dotenv"
+// dotenv.config()
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit process with failure
-    }
-};
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI);
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.error(`Error: ${error.message}`);
+//         process.exit(1); // Exit process with failure
+//     }
+// };
+
+// export default connectDB
+
+import {MongoClient, ServerApiVersion} from "mongodb"
+
+const uri = "mongodb+srv://mosimkhan15102001:gXbxNu8lIpr6czQQ@kabutar-cluster.utvb1.mongodb.net/?retryWrites=true&w=majority&appName=kabutar-cluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+}
+connectDB().catch(console.dir);
 
 export default connectDB
